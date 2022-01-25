858dfdcff61bbcaf9427be93b0a0b75f96aacdb3
sam15211521 <70865013+sam15211521@users.noreply.github.com>
2022-01-24 18:58:17 -0600

    added classes

--- Objects and classes/object and class game/game.py
+++ Objects and classes/object and class game/game.py
@@ -1,27 +1,53 @@
 import random
 
 
-class MC:
-    def __init__(self, name_input ='', skill_input, wepon_input, armor_input, health_points, magic_points) -> None:
-        self.name = name_input
-        self.skills = 
-        pass
+class MC: # class to create the main character. it gives his base health as well as 
+    def __init__(self, name_input ='', level, skill_input = None, weapon_input =None, armor_input =None, health_points=100, magic_points=20, attack=10):
+        self.name = name_input\
+        self.level = level
+        self.skills = skill_input
+        self.weapon= weapon_input
+        self.armor = armor_input 
+        self.health = health_points+ level*10
+        self.magic = magic_points +level*3
+        self.attack = attack + level
+        self.status = []
+
+
+    def move():
+        
 class Enemy:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name_input ='', skill_input = None, weapon_input =None, armor_input =None, health_points=100, magic_points=20):
+        self.name = name_input
+        self.skills = skill_input
+        self.weapon= weapon_input
+        self.armor = armor_input 
+        self.health = health_points
+        self.magic = magic_points
+        self.status = []
 class Item:
-    def __init__(self) -> None:
-        pass
-class weapon:
-    def __init__(self) -> None:
-        pass
-class obstical:
-    def __init__(self) -> None:
-        pass
-class armor:
-    def __init__(self) -> None:
-        pass
-class skill:
-    def __init__(self) -> None:
+    def __init__(self, wepon_damage, wepon_defence, weapon_type):
+        self.damage = wepon_damage
+        self.defence = wepon_defence
+        self.type = weapon_type
+class Weapon:
+    def __init__(self, wepon_damage, wepon_defence, weapon_type):
+        self.damage = wepon_damage
+        self.defence = wepon_defence
+        self.type = weapon_type
+class Obstical:
+    def __init__(self, tall, health_points) -> None:
+        self.heigth  = tall
+        self.health = health_points
+class Armor:
+    def __init__(self, DP, armor_type) -> None:
+        self.defence = DP
+        self.type = armor_type
+class Skill:
+    def __init__(self, target = 'Enemy', damage, healing, cost):
+        self.target = target
+        self.damage =damage
+        self.healing = healing
+        self.cost = cost 
         pass
 

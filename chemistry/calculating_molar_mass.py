e61b2b84f276f3246e87a25cdaff93e030962f94
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2022-03-31 14:22:49 -0500

    molar mass program now makes unballanced formulas and calculates molar mass

--- chemistry/calculating_molar_mass.py
+++ chemistry/calculating_molar_mass.py
@@ -1,3 +1,5 @@
+
+from logging import exception
 from periodictable import formula
 import periodictable
 
@@ -6,11 +8,18 @@ def find_molar_mass(compound): #finds the molar mass of a compound in grams
     return compound_mass
 
 def compounds_entered(i, reactants_or_products): #used to add the compounds to Enter_compounds
-    compound_string=f"Type the formula of the {i} {reactants_or_products} in your chemical equation. "
+    compound_string=f"Type the formula of the {i} {reactants_or_products} in your chemical equation. \n"
     compound = input(compound_string)
-    compound_upper = compound.upper()
-    compound_mass = find_molar_mass(compound_upper)
-    return {compound_upper: compound_mass}
+    while True:
+        try:            
+            compound_mass = find_molar_mass(compound)
+        except Exception:
+            compound = input(f'Error! Watch your capitlization of Element Symbols \n \n Please type the formula of the {i} {reactants_or_products} in your chemical equation. \n')
+        else:
+            return {compound: compound_mass}
+
+
+    
     
 
 def Enter_compounds(reactants_or_products = 'reactants'): #creates a dictionary of compounds present in the reaction equation
@@ -22,16 +31,26 @@ def Enter_compounds(reactants_or_products = 'reactants'): #creates a dictionary 
         compounds=compounds_entered(j, reactants_or_products)
         dict_of_compounds.update(compounds)
         j += 1
-        add_more = input('Do you have more to add? Y/N')
+        add_more = input('Do you have more to add? Y/N \n')
         add_more1 = add_more.upper()
-        print(add_more1)
         if add_more1 != 'Y' and add_more1 !='N':
-            add_more = input('Invalad Input!! \n do you have more to add? Y/N')
-        if add_more1 == 'N':
+            add_more = input('Invalad Input!! \n do you have more to add? Y/N \n')
+        elif add_more1 == 'N':
             reactant_flag =False 
             return dict_of_compounds
-        if add_more1 == 'Y':
+        elif add_more1 == 'Y':
             continue
+ 
+def making_chemical_equations(reactant_dictionary, product_dictionary):
+    equation_string =''
+    for key in reactant_dictionary:
+        equation_string += key
+        equation_string += ' + '
+    equation_string = equation_string[:-2] + '-> '
+    for key in product_dictionary:
+        equation_string += key +' + '
+    equation_string = equation_string[:-2]
+    return equation_string
 
 
 
@@ -41,9 +60,15 @@ def Enter_compounds(reactants_or_products = 'reactants'): #creates a dictionary 
 
 
 def main():
-    reactants = []
-    products = []
-    print(Enter_compounds())
+    reactants = Enter_compounds('reactants')
+    print(reactants)
+    print('\n Lets now enter products \n')
+    products = Enter_compounds('products')
+    
+    print(f'{reactants}\n \n{products}')
+    
+    chemical_formula_unballanced = making_chemical_equations(reactants, products)
+    print(chemical_formula_unballanced)
 
     
 

2f6c79ac3e452e52312d534a184f5756dee06a67
Sam1521 <70865013+sam15211521@users.noreply.github.com>
2022-04-27 16:37:58 -0500

    added practice for classes and objects, as well as puting in dicts

---
+++ Objects and classes/new class porject/class_intro.py
@@ -0,0 +1,48 @@
+class Elements:
+    number_of_element = 0
+
+    def __init__(self, symbol, name, mass):
+        self.name = name
+        self.symbol = symbol
+        self.mass = mass
+        Elements.number_of_element += 1
+    
+    def __del__(self):
+        print(f'Element {self.name} deleted')
+        Elements.number_of_element -= 1
+
+def making_elements():
+    lst_of_element_objects = {}
+    flag = True
+    while flag == True:
+        element_name = input('What is the name of the element\n\n')
+        element_symbol = input('What is the symbol of the element?\n\n')
+        element_mass = input('what is the mass of the element?\n\n')
+        while element_mass.replace('.', '', 1).isdigit() == False:
+            element_mass = input('what is the mass of the element?\n\n')
+        lst_of_element_objects[element_symbol] = Elements(element_symbol, element_name, float(element_mass))
+        
+        
+        next_element = input('do you have another element?   Y/N')
+        
+        
+        new_element = True
+        while new_element ==True:
+            if next_element.upper() == 'N':
+                flag = False
+                new_element = False
+            elif next_element.upper() == 'Y':
+                new_element = False
+            else:
+                continue
+    print(lst_of_element_objects)
+    return(lst_of_element_objects)
+
+
+    
+def main():
+    lst_of_elements = making_elements()
+
+    print(lst_of_elements['He'].mass)
+
+main()

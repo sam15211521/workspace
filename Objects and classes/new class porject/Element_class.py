a81db4c4d38c52f5210a462f14461b80cf1dfbbb
sam15211521 <samhendrixchemistry@gmail.com>
2022-04-30 06:11:09 -0700

    conitnuing with elementclass program

--- Objects and classes/new class porject/Element_class.py
+++ Objects and classes/new class porject/Element_class.py
@@ -1,4 +1,3 @@
-from time import sleep
 class Elements:
     number_of_element = 0
 
@@ -16,9 +15,211 @@ class Elements:
     
     def compare_electronegativities(element1, element2): # prints out the electronegativities of two elements
         print(f'The elements {element1.name} and {element2.name} have a mass of {element1.electronegativity} and {element2.electronegativity}')
-        
-    def electronegativities_prompt():
-        prompt0.1 = 'This '
-        prompt_1.1 = input("what is your first element?")
-        prompt_2 = input ('Do you want to do anything else with these elements?\n 1. Exit\n 2. Calculate the electronegativity diffrence and preditct type of chemical bond. \n\n')
-        if prompt_2
+    
+
+    #def element_look_up(elementname, elementsymbol):
+
+
+
+H = Elements('H', 'Hydrogen', 1.01, 1, 2.20)
+
+He = Elements('He','Helium',4.003,2, 0)
+
+Li = Elements('Li', 'Lithium', 6.941, 3, 0.98)
+
+Be = Elements('Be', 'Berylium' ,9.012182 , 4, 1.57)
+
+B = Elements('B', 'Boron' , 10.811, 5, 2.04)
+
+C = Elements('C', 'Carbon' ,12.0107 , 5, 2.55)
+
+N = Elements('N', 'Nitrogen' , 14.00674, 7, 2.04)
+
+O = Elements('O', 'Oxygen' , 15.9994, 8, 3.44)
+
+F = Elements('F', 'Florine' , 18.9984032, 9, 3.98)
+
+Ne = Elements('Ne', 'Neon' , 20.1797, 10, 0)
+
+Na = Elements('Na', 'Sodium' , 22.989770, 11, 0.93)
+
+Mg = Elements('Mg', 'Magnesium' , 24.3050, 12, 1.31)
+
+Al = Elements('Al', 'Alumnium' ,26.981538 , 13, 1.61)
+
+Si = Elements('Si', 'Silicon' , 28.0855, 14, 1.90)
+
+P = Elements('P', 'Phosphorus' , 30.973761, 15, 2.19)
+
+S  = Elements('S', 'Sulfur' ,32.066 , 16, 2.58)
+
+Cl = Elements('Cl', 'Chlorine' , 35.4527, 17, 3.16)
+
+Ar = Elements('Ar', 'Argon' , 39.948, 18, 0)
+
+K = Elements('K', 'Potassium' , 39.1983, 19, 0.82)
+
+Ca = Elements('Ca', 'Calcium' , 40.078, 20, 1.00)
+
+Sc = Elements('Sc', 'Scandium' , 44.955910, 21, 1.36)
+
+Ti = Elements('Ti', 'Titanium' , 47.867, 22, 1.54)
+
+V = Elements('V', 'Vanadium' , 50.9415, 23, 1.63)
+
+Cr = Elements('Cr', 'Chromium' , 51.9961, 24, 1.66)
+
+Mn = Elements('Mn', 'Manganese' , 54.938049, 25, 1.55)
+
+Fe = Elements('Fe', 'Iron' , 55.845, 26, 1.83)
+
+Co = Elements('Co', 'Cobalt' , 58.933200, 27, 1.88)
+
+Ni = Elements('Ni', 'Nickel' ,58.6934 , 28, 1.91)
+
+Cu = Elements('Cu', 'Copper' , 63.546, 29, 1.90)
+
+Zn = Elements('Zn', 'Zinc' , 65.39, 30, 1.65)
+
+Ga = Elements('Ga', 'Gallium' , 69.723, 31, 1.81)
+
+Ge = Elements('Ge', 'Germanium' , 72.61, 32, 2.01)
+
+As = Elements('As', 'Arsenic' , 74.2160, 33, 2.18)
+
+Se = Elements('Se', 'Selenium' , 78.96, 34, 2.55)
+
+Br = Elements('Br', 'Bromine' , 79.904, 35, 2.96)
+
+Kr = Elements('Kr', 'Krypton' , 83.80, 36, 3.00)
+
+Rb = Elements('Rb', 'Rubidium' , 85.4678, 37, 0.82)
+
+Sr = Elements('Sr', 'Strontium' , 87.62, 38, 0.95)
+
+Y = Elements('Y', 'Yittrium' , 88.90585, 39, 1.22)
+
+Zr = Elements('Zr', 'Zirconium' , 91.224, 40, 1.33)
+
+Nb = Elements('Nb', 'Niobium' , 92.90638, 41, 1.6)
+
+Mo = Elements('Mo', 'Molybdenum' , 95.94, 42, 2.16)
+
+Tc = Elements('Tc', 'Technetium' , 98, 43, 1.9)
+
+Ru = Elements('Ru', 'Ruthenium' , 101.07, 44, 2.2)
+
+Rh = Elements('Rh', 'Rhodium' , 102.90550, 45, 2.28)
+
+Pd = Elements('Pd', 'Palladium' , 106.42, 46, 2.20)
+
+Ag = Elements('Ag', 'Silver' , 107.8682, 47, 1.93)
+
+Cd = Elements('Cd', 'Cadmium' , 112.411, 48, 1.69)
+
+In = Elements('In', 'Indium' , 114.818, 49, 1.78)
+
+Sn = Elements('Sn', 'Tin' , 119.710, 50, 1.96)
+
+Sb = Elements('Sb', 'Antimony' , 121.760, 51, 2.05)
+
+Te = Elements('Te', 'Tellurium' , 127.60, 52, 2.1)
+
+I = Elements('Ru', 'Iodine' , 126.90447, 53, 2.66)
+
+Xe = Elements('Xe', 'Xenon' , 131.29, 54, 2.6)
+
+Cs = Elements('Cs', 'Cesium' , 132.90545, 55, 0.79)
+
+Ba = Elements('Ba', 'Barium' , 137.327, 56, 0.89)
+
+La = Elements('La', 'Lanthum' , 138.9055, 57, 1.10)
+
+Ce = Elements('Ce', 'Cerium' , 140.116, 58, 1.12)
+
+Pr = Elements('Pr', 'Praseodymium' , 140.90765, 59, 1.13)
+
+Nd = Elements('Nd', 'Neodymium' , 144.24, 60, 1.14)
+
+Pm = Elements('Pm', 'Promethium' , 145, 61, 1.13)
+
+Sm = Elements('Sm', 'Samarium' , 150.36, 62, 1.17)
+
+Eu = Elements('Eu', 'Europium' , 151.964, 63, 1.2)
+
+Gd = Elements('Gd', 'Gadolinium' , 157.25, 64, 1.2)
+
+Tb = Elements('Tb', 'Terbium' , 158.92534, 65, 1.22)
+
+Dy = Elements('Dy', 'Dysprosium' , 162.50, 66, 1.23)
+
+Ho = Elements('Ho', 'Holmium' , 164.93032, 67, 1.24)
+
+Er = Elements('Er', 'Erbium' , 167.26, 68, 1.24)
+
+Tm = Elements('Tm', 'thulium' , 168.93421, 69, 1.25)
+
+Yb = Elements('Yb', 'Ytterbium' , 173.04, 70, 1.1)
+
+Lu = Elements('Lu', 'Lutetium' , 174.967, 71, 1.27)
+
+Hf = Elements('Hf', 'Hafnium' , 178.49, 72, 1.3)
+
+Ta = Elements('Ta', 'Tantalum' , 180.9479, 73, 1.5)
+
+W= Elements('W', 'Tungsten' , 183.84, 74, 2.36)
+
+Re = Elements('Re', 'Rhenium' , 186.207, 75, 1.9)
+
+Os = Elements('Os', 'Osmium' , 190.23, 76, 2.2)
+
+Ir = Elements('Ir', 'Iridium' , 192.217, 77, 2.2)
+
+Pt = Elements('Pt', 'Platinum' , 195.078, 78, 2.28)
+
+Au = Elements('Au', 'Gold' , 196.96655, 79, 2.54)
+
+Hg = Elements('Hg', 'Mercury' , 200.59, 80, 2.00)
+
+Tl = Elements('Tl', 'Thallium' , 204.3833, 81, 1.62)
+
+Pb = Elements('Pb', 'Lead' , 207.2, 82, 2.33)
+
+Bi = Elements('Bi', 'Bismuth' , 208.98038, 83, 2.02)
+
+Po = Elements('Po', 'Polonium' , 209, 84, 2.0)
+
+At = Elements('At', 'Astatine' , 210, 85, 2.2)
+
+Rn = Elements('Rn', 'Radon' , 222, 86, 0)
+
+Fr = Elements('Fr', 'Francium' , 223, 87, 0.7)
+
+Ra = Elements('Ra', 'Radium' , 226, 88, 0.89)
+
+Ac = Elements('Ac', 'Actinium' , 227, 89, 1.1)
+
+Th = Elements('Th', 'Thorium' , 232.0381, 90, 1.3)
+
+Pa = Elements('Pa', 'Protactinium' , 231.03588, 91, 1.5)
+
+U = Elements('U', 'Uranium' , 238.0289, 92, 1.38)
+
+Np = Elements('Np', 'Neptunium' , 237, 93, 1.38)
+
+Pu = Elements('Pu', 'Plutonium' , 244, 94, 1.28)
+
+Am = Elements('Am', 'Americium' , 243, 95, 1.3)
+
+Cm = Elements('Cm', 'Curium' , 247, 96, 1.3)
+
+Bk = Elements('Bk', 'Berkelium' , 247, 97, 1.3)
+
+Cf = Elements('Cf', 'Californium' , 251, 98, 1.3)
+
+element_list =[H, He, Li, Be, B, C, N, O, F, Ne, Na, Mg, Al, Si, P, S, Cl, Ar, K, Ca, Sc, Ti, V, Cr, Mn, Fe, Co, Ni, Cu, Zn, Ga, Ge, As, Se, Br, Kr, Rb, Sr, Y, Zr, Nb, Mo, Tc, Ru, Rh, Pd, Ag, Cd, In, Sn, Sb, Te, I, Xe, Cs, Ba, La, Ce, Pr, Nd, Pm, Sm, Eu, Gd, Tb, Dy, Ho, Er, Tm, Yb, Lu, Hf, Ta, W, Re, Os, Ir, Pt, Au, Hg, Tl, Pb, Bi, Po, At, Rn, Fr, Ra, Th, Pa, U , Np, Pu, Am, Cm, Bk, Cf]
+
+#for abc in element_list:
+#    print(abc.name)
+
+
---
+++ Objects and classes/new class porject/main.py
@@ -0,0 +1,36 @@
+import Element_class
+from time import sleep
+def finding_element_name
+
+def electronegativities_prompt(): #this belongs not as a method but as a function
+        print('This program compairs two elements and gives their electronegativity diffrences,\nas well as their predicted bond type')
+        sleep(2)
+        prompt1_1text = 'What do you want to do? (type the number):\n 1) Enter elements \n 2)Exit\n\n'
+        prompt1_1 = input(prompt1_1text)
+
+        while prompt1_1 != '1' and prompt1_1 != '2':
+            prompt1_1 = input('Please enter a number: \n 1) Enter elements \n 2)Exit\n\n')
+        else:
+            if prompt1_1 == '1':
+                prompt1_flag = True
+                while prompt1_flag == True:
+                    first_element = input('Please input the symbol or name of the first element you wish to compare.')
+                    second_element = input('Please input the symbol or name of the second element you wish to compare.')
+                    
+                   #statment = f'The elements you have chosen are {}, ({}) and {}, ({})'
+
+            elif prompt1_1 == '2':
+                return False
+
+def main():
+    electronegativities_prompt()
+
+#main()
+#c = input('type an element symbol')
+
+aaa = 'H'
+
+for ele in Element_class.element_list:
+    if aaa == ele.symbol:
+        print(ele.name)
+    break
